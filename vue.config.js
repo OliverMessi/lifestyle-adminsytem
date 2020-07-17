0ba361a886b2db0910226dedb88c16ea00d249a4
OliverMessi <562376662@qq.com>
2020-07-17 16:32:53 +0800

    '创建vue-cli3项目'

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  devServer: {
+    open: true, //配置自动启动浏览器
+    port: 8002,// 项目运行时候的端口号
+    hotOnly: true, // 热更新
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:8443',
+    //     changeOrigin: true,
+    //     ws: true,
+    //     pathRewrite: {
+    //       '^/api': ''
+    //     }
+    //   }
+    // }
+  },
+  configureWebpack:{
+    resolve:{
+      alias:{
+        'assets':'@/assets',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network',
+        'views':'@/views',
+      }
+    },
+    externals: {
+        'AMap': 'AMap' // 高德地图配置
+    }
+  },
+};
