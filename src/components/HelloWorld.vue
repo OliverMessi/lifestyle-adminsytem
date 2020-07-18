4f0899377e6937a9d1069948b9d08ae6b9f12e84
OliverMessi <562376662@qq.com>
2020-07-18 09:59:20 +0800

    引入elementUI

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,6 +12,7 @@
     </ul>
     <h3>Essential Links</h3>
     <ul>
+      <el-button type="primary">主要按钮</el-button>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
