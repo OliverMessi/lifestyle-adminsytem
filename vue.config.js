fe747b53afa09566b27755106c0855a8c5a3ad8e
OliverMessi <562376662@qq.com>
2020-07-19 10:19:51 +0800

    安装axios以及mock.js

--- vue.config.js
+++ vue.config.js
@@ -3,20 +3,21 @@ module.exports = {
     open: true, //配置自动启动浏览器
     port: 8002,// 项目运行时候的端口号
     hotOnly: true, // 热更新
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:8443',
-    //     changeOrigin: true,
-    //     ws: true,
-    //     pathRewrite: {
-    //       '^/api': ''
-    //     }
-    //   }
-    // }
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8002',
+        changeOrigin: true,
+        ws: true,
+        // pathRewrite: {
+        //   '^/api': ''
+        // }
+      }
+    },
   },
   configureWebpack:{
     resolve:{
       alias:{
+        'mock':'@/mock',
         'assets':'@/assets',
         'common':'@/common',
         'components':'@/components',
