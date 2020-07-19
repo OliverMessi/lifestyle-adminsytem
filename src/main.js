96ef2ea5ddafaee2edf00aa63b52ee32323c1fa1
OliverMessi <562376662@qq.com>
2020-07-19 22:10:19 +0800

    项目国际化完成

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 import mock from 'mock/index'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 new Vue({
   render: h => h(App),
-  router
+  router,
+  i18n
 }).$mount('#app')
